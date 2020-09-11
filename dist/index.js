552fc6c90b6988ba03e6605d191214da37abe7ef
hyunjin-jeong <hyun0524e@naver.com>
2020-09-11 17:38:40 +0900

    Completed TypeChain

--- dist/index.js
+++ dist/index.js
@@ -1,5 +1,6 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
+const cryptoJS = require("crypto-js");
 class block {
     constructor(index, hash, previousHash, data, timestamp) {
         this.index = index;
@@ -9,7 +10,57 @@ class block {
         this.timestamp = timestamp;
     }
 }
+block.calculateBlockHash = (index, previousHash, timestamp, data) => cryptoJS.SHA256(index + previousHash + timestamp + data).toString();
+block.validateStructure = (aBlock) => typeof aBlock.index === "number" &&
+    typeof aBlock.hash === "string" &&
+    typeof aBlock.previousHash === "string" &&
+    typeof aBlock.timestamp === "number" &&
+    typeof aBlock.data === "string";
 const genesisBlock = new block(0, "32132131243", "", "Hello", 123456);
 let blockChain = [genesisBlock];
+const getBlockChain = () => blockChain;
+const getLatestBlock = () => blockChain[blockChain.length - 1];
+const getNewTimestamp = () => Math.round(new Date().getTime() / 1000);
+const createNewBlock = (data) => {
+    const previousBlock = getLatestBlock();
+    const newIndex = previousBlock.index + 1;
+    const newTimestamp = getNewTimestamp();
+    const newHash = block.calculateBlockHash(newIndex, previousBlock.hash, newTimestamp, data);
+    const newBlock = new block(newIndex, newHash, previousBlock.hash, data, newTimestamp);
+    addBlock(newBlock);
+    return newBlock;
+};
+const getHashforBlock = (aBlock) => block.calculateBlockHash(aBlock.index, aBlock.previousHash, aBlock.timestamp, aBlock.data);
+// 블록 유효성을 검사하는 함수이다.
+const isBlockValid = (candidateBlock, previousBlock) => {
+    // 블록의 구조가 유효하지 않으면 false
+    if (!block.validateStructure(candidateBlock)) {
+        return false;
+        // 이전 블록의 인덱스 + 1과 다르면 false
+    }
+    else if (previousBlock.index + 1 !== candidateBlock.index) {
+        return false;
+        // 이전 블록의 hash와 다르면 false
+    }
+    else if (previousBlock.hash !== candidateBlock.previousHash) {
+        return false;
+        // 다른 hash를 갖고 있다면 false
+    }
+    else if (getHashforBlock(candidateBlock) !== candidateBlock.hash) {
+        return false;
+        // 모든 검증을 통과했을 때 true
+    }
+    else {
+        return true;
+    }
+};
+const addBlock = (candidateBlock) => {
+    if (isBlockValid(candidateBlock, getLatestBlock())) {
+        blockChain.push(candidateBlock);
+    }
+};
+createNewBlock("second block");
+createNewBlock("third block");
+createNewBlock("fourth block");
 console.log(blockChain);
 //# sourceMappingURL=index.js.map
--- src/index.ts
+++ src/index.ts
@@ -15,6 +15,13 @@ class block {
     ): string =>
         cryptoJS.SHA256(index + previousHash + timestamp + data).toString();
 
+    static validateStructure = (aBlock: block): boolean =>
+        typeof aBlock.index === "number" &&
+        typeof aBlock.hash === "string" &&
+        typeof aBlock.previousHash === "string" &&
+        typeof aBlock.timestamp === "number" &&
+        typeof aBlock.data === "string";
+
     constructor(index: number,
         hash: string,
         previousHash: string,
@@ -38,5 +45,64 @@ const getBlockChain = (): block[] => blockChain;
 const getLatestBlock = (): block => blockChain[blockChain.length - 1];
 
 const getNewTimestamp = (): number => Math.round(new Date().getTime() / 1000);
+
+const createNewBlock = (data: string): block => {
+    const previousBlock: block = getLatestBlock();
+    const newIndex: number = previousBlock.index + 1;
+    const newTimestamp: number = getNewTimestamp();
+    const newHash: string = block.calculateBlockHash(
+        newIndex,
+        previousBlock.hash,
+        newTimestamp,
+        data
+    );
+    const newBlock: block = new block(
+        newIndex,
+        newHash,
+        previousBlock.hash,
+        data,
+        newTimestamp
+    );
+    addBlock(newBlock);
+    return newBlock
+};
+
+const getHashforBlock = (aBlock: block): string => block.calculateBlockHash(aBlock.index, aBlock.previousHash, aBlock.timestamp, aBlock.data);
+
+// 블록 유효성을 검사하는 함수이다.
+const isBlockValid = (candidateBlock: block, previousBlock: block): boolean => {
+    // 블록의 구조가 유효하지 않으면 false
+    if (!block.validateStructure(candidateBlock)){
+        return false;
+
+    // 이전 블록의 인덱스 + 1과 다르면 false
+    } else if (previousBlock.index + 1 !== candidateBlock.index){
+        return false;
+
+    // 이전 블록의 hash와 다르면 false
+    } else if (previousBlock.hash !== candidateBlock.previousHash){
+        return false;
+
+    // 다른 hash를 갖고 있다면 false
+    } else if (getHashforBlock(candidateBlock) !== candidateBlock.hash){
+        return false;
+
+    // 모든 검증을 통과했을 때 true
+    } else {
+        return true;
+    }
+};
+
+const addBlock = (candidateBlock: block): void => {
+    if (isBlockValid(candidateBlock, getLatestBlock())){
+        blockChain.push(candidateBlock);
+    }
+}
+
+createNewBlock("second block");
+createNewBlock("third block");
+createNewBlock("fourth block");
+
+console.log(blockChain);
 
 export {};
